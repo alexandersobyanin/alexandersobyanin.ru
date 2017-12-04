77bf5780fb9e822b566ef77dcd2257383d49eec8
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2017-12-05 04:03:55 +0500

    check https

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var block_home_page = $('#block-home-page');
 	block_home_page.hide();
-	$.getJSON('http://b1oki.noip.me/health.php', function (data) {
+	$.getJSON('https://b1oki.noip.me/health.php', function (data) {
 		console.log('begin data');
 		console.log(data);
 		console.log('finish data');
