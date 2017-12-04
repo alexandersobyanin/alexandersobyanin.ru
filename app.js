8bb094de422477c345dbdac4739c3ce045fb7a43
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2017-12-05 04:17:53 +0500

    prefer https

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var block_home_page = $('#block-home-page');
-	$.getJSON('//b1oki.noip.me/health.php', function (data) {
+	$.getJSON('http://b1oki.noip.me/health.php', function (data) {
 		console.log('begin data');
 		console.log(data);
 		console.log('finish data');
