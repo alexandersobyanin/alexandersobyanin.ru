ba2e5336eccbd623522a00509444e1a7c6d1f21c
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2017-12-05 04:08:24 +0500

    fix https

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var block_home_page = $('#block-home-page');
 	block_home_page.hide();
-	$.getJSON('https://b1oki.noip.me/health.php', function (data) {
+	$.getJSON('//b1oki.noip.me/health.php', function (data) {
 		console.log('begin data');
 		console.log(data);
 		console.log('finish data');
