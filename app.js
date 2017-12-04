501203ca439d30eb946bfa8f56a97cf05ad5ead2
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2017-12-05 04:14:16 +0500

    hide link only if js works

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
 	var block_home_page = $('#block-home-page');
-	block_home_page.hide();
 	$.getJSON('//b1oki.noip.me/health.php', function (data) {
 		console.log('begin data');
 		console.log(data);
 		console.log('finish data');
-		if (data.health == 1) {
-			block_home_page.show();
+		if (data.health != 1) {
+			block_home_page.hide();
 		}
 	});
 });
