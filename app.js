ff034216ab5039e2745a01aa542ed8179e6289ef
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2018-04-07 18:09:42 +0500

    remove trash

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 	if (location.host != "alexandersobyanin.ru") {
 		location.replace("http://alexandersobyanin.ru");
 	}
@@ -8,24 +7,15 @@ $(document).ready(function() {
 	}
 
 	var block_home_page = $('#block-home-page');
-
 	$.ajax({
 		url: 'http://b1oki.noip.me/health.php',
 		dataType: 'json',
 		success: function (data) {
-			console.log('get success');
-			console.log('begin data');
-			console.log(data);
-			console.log('finish data');
 			if (data.health != 1) {
 				block_home_page.hide();
 			}
 		},
 		error: function (data) {
-			console.log('get error');
-			console.log('begin data');
-			console.log(data);
-			console.log('finish data');
 			block_home_page.hide();
 		}
 	});
