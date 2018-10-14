43668491af2649dc32b92f2eddf5d91e784640e5
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2018-10-14 21:10:38 +0500

    Moved to react

---
+++ precache-manifest.5099fde0f4218d9036ae058a98e62247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96eec86976e9d27903ca",
+    "url": "/static/js/main.96eec869.chunk.js"
+  },
+  {
+    "revision": "dad035d15cbac9713c9b",
+    "url": "/static/js/1.dad035d1.chunk.js"
+  },
+  {
+    "revision": "96eec86976e9d27903ca",
+    "url": "/static/css/main.43fa3804.chunk.css"
+  },
+  {
+    "revision": "a939525cada282f391dc987ded111ce0",
+    "url": "/index.html"
+  }
+];
