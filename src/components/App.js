fc69cf2b4db21ed92a2b642675b79e8caae7dba8
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2018-11-04 05:23:52 +0500

    move react files from old repo

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Article from './Article'
+import articles from '../fixtures'
+
+function App() {
+    return null;
+    /*return (
+        <div>
+            <Article article={articles[0]} />
+        </div>
+    )*/
+}
+
+export default App
