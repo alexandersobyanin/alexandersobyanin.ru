0a7cb934e0890d8f6534622244de0b095a37294b
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-10-04 21:14:52 +0500

    Feat: prepare another FCM way

--- src/components/notifications/Notifications.js
+++ src/components/notifications/Notifications.js
@@ -1,16 +1,14 @@
 import React, {Component} from "react";
 import './Notifications.css';
-import {messaging} from "../firebase";
 
 class Notifications extends Component {
     render() {
-        let token = messaging.getToken();
         return (
             <div>
-                <h1>React + Firebase Cloud Messaging (Push Notifications)</h1>
-                <div>
-                    Current token is: <p>{token}</p>
-                </div>
+                <p>Firebase Cloud Messaging</p>
+                <button onClick="">
+                    Enable push notifications
+                </button>
             </div>
         )
     }
