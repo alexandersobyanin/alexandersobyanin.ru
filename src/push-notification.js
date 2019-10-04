d7ea282bb27273a6cf963c06693eabc5e0e84f3e
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-10-04 23:54:32 +0500

    Feat: FCM problem

--- src/push-notification.js
+++ src/push-notification.js
@@ -11,17 +11,28 @@ export const initializeFirebase = () => {
 export const askForPermissioToReceiveNotifications = async () => {
     console.log('Requesting messaging permission');
     const messaging = firebase.messaging();
-    messaging.usePublicVapidKey("BC5iFJKADo-XFlYbbgV8UAu4cDTfzfyZxr5lBxeJBDItFv1m7TchiWbYMgeJJMKNFPxNahtLtg1nNdgdgR1yW4c");
-    messaging.requestPermission().then(() => {
-        if (Notification.permission === 'granted') {
+    Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
             console.log('user has granted permissions');
             messaging.onTokenRefresh(token => {
                 console.log('onTokenRefresh token: ', token);
+                console.log('use vapid key');
+                messaging.usePublicVapidKey("BC5iFJKADo-XFlYbbgV8UAu4cDTfzfyZxr5lBxeJBDItFv1m7TchiWbYMgeJJMKNFPxNahtLtg1nNdgdgR1yW4c");
+                console.log('used vapid key');
+                messaging.getToken().then((refreshedToken) => {
+                    console.log('Token refreshed: ', refreshedToken);
+                    // Indicate that the new Instance ID token has not yet been sent to the app server.
+                    // Send Instance ID token to app server.
+                }).catch((err) => {
+                    console.log('Unable to retrieve refreshed token ', err);
+                });
+                console.log('onTokenRefresh finish');
             });
         } else {
             console.log('user has rejected permissions');
         }
-    }).catch(error => {
-        console.error('requestPermission error: ', error);
+    }).catch(function (error) {
+        console.log("Unable to get permission to notify: ", error);
     });
+    navigator.serviceWorker.addEventListener("messasge", (message) => console.log(message));
 };
