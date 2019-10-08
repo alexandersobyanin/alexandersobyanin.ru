f1735668b97f228426888a7c7ba1846616128655
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-10-08 10:32:33 +0500

    Refactoring: format js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { compose } from 'recompose';
+import {compose} from 'recompose';
 
-import { withAuthorization, withEmailVerification } from '../Session';
+import {withAuthorization, withEmailVerification} from '../Session';
 import Messages from '../Messages';
 
 const HomePage = () => (
-  <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
+    <div>
+        <h1>Home Page</h1>
+        <p>The Home Page is accessible by every signed in user.</p>
 
-    <Messages />
-  </div>
+        <Messages/>
+    </div>
 );
 
 const condition = authUser => !!authUser;
 
 export default compose(
-  withEmailVerification,
-  withAuthorization(condition),
+    withEmailVerification,
+    withAuthorization(condition),
 )(HomePage);
