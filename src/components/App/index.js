4dc07c65afc4bbad856061184f712e12e09bbdfb
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-10-20 02:31:22 +0500

    Feat: 404 error page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 
 import {YMInitializer} from "react-yandex-metrika";
 import ReactGA from 'react-ga';
 import ErrorBoundary from "../ErrorBoundary";
+import FourOhFour from "../FourOhFour";
 import Header from '../Header';
 import Footer from "../Footer";
 import LandingPage from '../Landing';
@@ -26,13 +27,17 @@ const App = () => (
         <Router basename={process.env.PUBLIC_URL}>
             <Header/>
             <main>
-                <Route exact path={ROUTES.LANDING} component={LandingPage}/>
-                <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
-                <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
-                <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
-                <Route path={ROUTES.HOME} component={HomePage}/>
-                <Route path={ROUTES.ACCOUNT} component={AccountPage}/>
-                <Route path={ROUTES.ADMIN} component={AdminPage}/>
+                <Switch>
+                    <Route exact strict path="/:url*" render={props => <Redirect to={`${props.location.pathname}/`}/>}/>
+                    <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+                    <Route exact path={ROUTES.SIGN_UP} component={SignUpPage}/>
+                    <Route exact path={ROUTES.SIGN_IN} component={SignInPage}/>
+                    <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
+                    <Route exact path={ROUTES.HOME} component={HomePage}/>
+                    <Route exact path={ROUTES.ACCOUNT} component={AccountPage}/>
+                    <Route exact path={ROUTES.ADMIN} component={AdminPage}/>
+                    <Route component={FourOhFour}/>
+                </Switch>
             </main>
             <Footer/>
             <YMInitializer
---
+++ src/components/FourOhFour/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FourOhFour = () => (
+    <div>
+        404
+    </div>
+);
+
+export default FourOhFour;
