0104a17de25d9607569b1c0fce14e42bd0de6961
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-10-21 02:53:40 +0500

    Fix: admin detail page returns 404

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -30,12 +30,12 @@ const App = () => (
                                render={props => <Redirect to={`${props.location.pathname}/`}/>}
                         />
                         <Route exact path={ROUTES.LANDING} component={LandingPage}/>
-                        <Route exact path={ROUTES.SIGN_UP} component={SignUpPage}/>
-                        <Route exact path={ROUTES.SIGN_IN} component={SignInPage}/>
-                        <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
-                        <Route exact path={ROUTES.HOME} component={HomePage}/>
-                        <Route exact path={ROUTES.ACCOUNT} component={AccountPage}/>
-                        <Route exact path={ROUTES.ADMIN} component={AdminPage}/>
+                        <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
+                        <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
+                        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
+                        <Route path={ROUTES.HOME} component={HomePage}/>
+                        <Route path={ROUTES.ACCOUNT} component={AccountPage}/>
+                        <Route path={ROUTES.ADMIN} component={AdminPage}/>
                         <Route component={FourOhFour}/>
                     </Switch>
                 </Container>
