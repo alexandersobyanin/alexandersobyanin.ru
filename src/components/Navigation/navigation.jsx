680b361ee000e94cc83dd15f9cf8f34e10e6fe58
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-10-25 03:30:49 +0500

    Feat: icons in navbar

--- src/components/Navigation/navigation.jsx
+++ src/components/Navigation/navigation.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import {NavLink as RRNavLink} from 'react-router-dom';
 import {Nav, NavItem, NavLink} from "reactstrap";
+import {
+    FiHome as IconHome,
+    FiMessageSquare as IconChat,
+    FiCodepen as IconProject,
+    FiList as IconLists,
+    FiUser as IconAccount,
+    FiAlertTriangle as IconAdmin,
+    FiLogIn as IconSignIn
+} from 'react-icons/fi';
 
 import {AuthUserContext} from '../Session';
 import SignOutButton from '../SignOut';
@@ -22,23 +31,35 @@ const Navigation = () => (
 const NavigationAuth = ({authUser}) => (
     <Nav className="ml-auto" navbar>
         <NavItem>
-            <NavLink exact to={ROUTES.HOME} tag={RRNavLink}>Home</NavLink>
+            <NavLink exact to={ROUTES.HOME} tag={RRNavLink}>
+                <IconHome title="Home"/><span className="d-none d-sm-inline"> Home</span>
+            </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink to={ROUTES.CHAT} tag={RRNavLink}>Chat</NavLink>
+            <NavLink to={ROUTES.CHAT} tag={RRNavLink}>
+                <IconChat title="Chat"/><span className="d-none d-sm-inline"> Chat</span>
+            </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink href={ROUTES.PROJECTS}>Projects</NavLink>
+            <NavLink href={ROUTES.PROJECTS}>
+                <IconProject title="Projects"/><span className="d-none d-sm-inline"> Projects</span>
+            </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink href={ROUTES.LISTS}>Lists</NavLink>
+            <NavLink href={ROUTES.LISTS}>
+                <IconLists title="Lists"/><span className="d-none d-sm-inline"> Lists</span>
+            </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink to={ROUTES.ACCOUNT} tag={RRNavLink}>Account</NavLink>
+            <NavLink to={ROUTES.ACCOUNT} tag={RRNavLink}>
+                <IconAccount title="Account"/><span className="d-none d-sm-inline"> Account</span>
+            </NavLink>
         </NavItem>
         {!!authUser.roles[ROLES.ADMIN] && (
             <NavItem>
-                <NavLink to={ROUTES.ADMIN} tag={RRNavLink}>Admin</NavLink>
+                <NavLink to={ROUTES.ADMIN} tag={RRNavLink}>
+                    <IconAdmin title="Admin"/><span className="d-none d-sm-inline"> Admin</span>
+                </NavLink>
             </NavItem>
         )}
         <NavItem>
@@ -50,13 +71,19 @@ const NavigationAuth = ({authUser}) => (
 const NavigationNonAuth = () => (
     <Nav className="ml-auto" navbar>
         <NavItem>
-            <NavLink exact to={ROUTES.HOME} tag={RRNavLink}>Home</NavLink>
+            <NavLink exact to={ROUTES.HOME} tag={RRNavLink}>
+                <IconHome title="Home"/><span className="d-none d-sm-inline"> Home</span>
+            </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink href={ROUTES.PROJECTS}>Projects</NavLink>
+            <NavLink href={ROUTES.PROJECTS}>
+                <IconProject title="Projects"/><span className="d-none d-sm-inline"> Projects</span>
+            </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink to={ROUTES.SIGN_IN} tag={RRNavLink}>Sign In</NavLink>
+            <NavLink to={ROUTES.SIGN_IN} tag={RRNavLink}>
+                <IconSignIn title="Sign In"/><span className="d-none d-sm-inline"> Sign In</span>
+            </NavLink>
         </NavItem>
     </Nav>
 );
--- src/components/SignOut/signOut.jsx
+++ src/components/SignOut/signOut.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Button} from "reactstrap";
+import {FiLogOut as IconSignOut} from 'react-icons/fi';
 
 import {withFirebase} from '../Firebase';
 
 const SignOutButton = ({firebase}) => (
     <Button onClick={firebase.doSignOut}>
-        Sign Out
+        <IconSignOut title="Sign Out"/><span className="d-none d-sm-inline"> Sign Out</span>
     </Button>
 );
 
