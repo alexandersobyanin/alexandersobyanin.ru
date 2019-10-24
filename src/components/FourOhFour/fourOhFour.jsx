4eecc5f075a63b0f7ac9665ebb5eca4e55d056b1
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-10-25 03:21:38 +0500

    Feat: improve 404 page

--- src/components/FourOhFour/fourOhFour.jsx
+++ src/components/FourOhFour/fourOhFour.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {Fragment} from "react";
+import {Link} from "react-router-dom";
+
+import * as ROUTES from "../../constants/routes"
 
 const FourOhFour = () => (
-    <div>
-        404
-    </div>
+    <Fragment>
+        <h1>404</h1>
+        <h2>Page or file not found</h2>
+        <p>
+            We're sorry but the page you're looking for is unavailable, was renamed or no longer exists.
+            Also, please be sure that you're using the correct URL.
+        </p>
+        <Link to={ROUTES.HOME} className="btn btn-secondary">Return</Link>
+    </Fragment>
 );
 
 export default FourOhFour;
