9ec0a7da485c655c1eab7be1361baea00de3bd19
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2020-05-05 04:44:45 +0500

    Feat: Google FCM

--- src/components/Notifications/notifications.jsx
+++ src/components/Notifications/notifications.jsx
@@ -1,42 +1,50 @@
-import React from "react";
+import React, {Component} from "react";
 import {Button} from "reactstrap";
 import 'firebase/messaging';
 import * as firebase from 'firebase/app';
 
-const askForPermissioToReceiveNotifications = async () => {
-    console.log('Requesting messaging permission');
-    const messaging = firebase.messaging();
-    Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') {
-            console.log('user has granted permissions');
-            console.log('use vapid key');
-            messaging.usePublicVapidKey("BC5iFJKADo-XFlYbbgV8UAu4cDTfzfyZxr5lBxeJBDItFv1m7TchiWbYMgeJJMKNFPxNahtLtg1nNdgdgR1yW4c");
-            console.log('used vapid key');
-            messaging.getToken().then((refreshedToken) => {
-                console.log('Token refreshed: ', refreshedToken);
-                // Indicate that the new Instance ID token has not yet been sent to the app server.
-                // Send Instance ID token to app server.
-                console.log('getToken finish');
-            }).catch((err) => {
-                console.log('Unable to retrieve refreshed token ', err);
-            });
-            console.log('after getToken');
-        } else {
-            console.log('user has rejected permissions');
-        }
-    }).catch(function (error) {
-        console.log("Unable to get permission to notify: ", error);
-    });
-    navigator.serviceWorker.addEventListener("message", (message) => console.log(message));
-};
+import {withFirebase} from "../Firebase";
 
-const Notifications = () => (
-    <div>
-        <h2>Firebase Cloud Messaging</h2>
-        <Button onClick={askForPermissioToReceiveNotifications}>
-            Enable push notifications
-        </Button>
-    </div>
-);
+class Notifications extends Component {
+    vapidKey = process.env.REACT_APP_FIREBASE_PUBLIC_VAPID_KEY;
 
-export default Notifications
+    askForPermissioToReceiveNotifications = async () => {
+        console.log('Requesting messaging permission');
+        const messaging = firebase.messaging();
+        Notification.requestPermission().then((permission) => {
+            if (permission === 'granted') {
+                console.log('user has granted permissions');
+                console.log('use vapid key');
+                messaging.usePublicVapidKey(this.vapidKey);
+                console.log('used vapid key');
+                messaging.getToken().then((refreshedToken) => {
+                    console.log('Token refreshed: ', refreshedToken);
+                    // Indicate that the new Instance ID token has not yet been sent to the app server.
+                    // Send Instance ID token to app server.
+                    console.log('getToken finish');
+                }).catch((err) => {
+                    console.log('Unable to retrieve refreshed token ', err);
+                });
+                console.log('after getToken');
+            } else {
+                console.log('user has rejected permissions');
+            }
+        }).catch(function (error) {
+            console.log("Unable to get permission to notify: ", error);
+        });
+        navigator.serviceWorker.addEventListener("message", (message) => console.log(message));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Firebase Cloud Messaging</h2>
+                <Button onClick={this.askForPermissioToReceiveNotifications}>
+                    Enable push notifications
+                </Button>
+            </div>
+        );
+    }
+}
+
+export default withFirebase(Notifications);
