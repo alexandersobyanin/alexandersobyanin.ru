ad7a8f550fe8c2f1282b745f61ca1e2dd9209a4e
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2020-05-05 03:34:23 +0500

    Add: link to LinkedIn in Portfolio section

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -3,6 +3,10 @@ import React, {Fragment} from 'react';
 const Portfolio = () => (
     <Fragment>
         <h2>Portfolio</h2>
+        <p>
+            In progress...
+            <a href="https://linkedin.com/in/alexandersobyanin">Temporary link</a>
+        </p>
     </Fragment>
 );
 
